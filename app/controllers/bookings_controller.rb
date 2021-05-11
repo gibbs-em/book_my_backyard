a610c352461feed9d13f3a025f302671a110925e
Daniella Mogilner <daniella.m@hotmail.co.uk>
2021-05-11 21:25:14 +0100

    added a destroy to booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -19,6 +19,12 @@ class BookingsController < ApplicationController
     @bookings = Booking.all
   end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
   private
 
   def booking_params
