f27cc13f7a5fc54d20e5cc7aa37216fd07d1f178
Thomas Loubser <taloubser@gmail.com>
2021-05-15 12:30:33 +0100

    re-added garden model has :photo after conflicts

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,6 +2,7 @@ class Garden < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings, dependent: :destroy
+  has_one_attached :photo
 
   validates :title,  presence: true
   validates :description, presence: true
