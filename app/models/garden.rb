3e705985cf99d335a44b542dff91ddc61145b07e
Thomas Loubser <taloubser@gmail.com>
2021-05-15 11:14:26 +0100

    updated config for cloudinary, and garden model

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -2,6 +2,7 @@ class Garden < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings
+  has_one_attached :photo
 
   validates :title,  presence: true
   validates :description, presence: true
