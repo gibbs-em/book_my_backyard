dc59eb471829737b7d7d15b6fdb63f1f3e9b241e
Thomas Loubser <taloubser@gmail.com>
2021-05-15 15:39:18 +0100

    updated seed file and defulat image

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 puts 'Creating fake gardens & reviews...'
 if Rails.env.development?
   Garden.destroy_all
+  Booking.destroy_all
+  Review.destroy_all
 end
 
 ["10 Downing Street", "221B Baker Street", "48 Doughty Street", "25 Brook Street", "186 Fleet Street", "71 Salford Road", "59A Fairlight Road"].each do |address|
@@ -10,14 +12,13 @@ end
     address: "#{address}, London",
     user_id: 1)
   garden.save!
+  puts "#{garden.title} created!"
+  booking = Booking.new(garden_id: garden.id, user_id: 1)
+  booking.save!
+  puts "Booking id:#{booking.id} created!"
+  review = Review.new(title: 'Great Place!', description: 'I would stay here again', rating: 4, booking_id: booking.id )
+  review.save!
+  puts "#{review.title} created!"
 end
 
-3.times do
-  review = Review.new(
-    title: 'Great Place!',
-    description: 'I would stay here again',
-    rating: 4,
-    booking_id: 1
-    )
-end
 puts 'Finished!'
